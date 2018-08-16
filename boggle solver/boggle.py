16f5cd674c509793730a779338934efc537c6003
Stephen Macgregor <33063078+Krom121@users.noreply.github.com>
2018-08-16 21:51:14 +0100

    added new tests and function to boggle

--- boggle solver/boggle.py
+++ boggle solver/boggle.py
@@ -1,8 +1,43 @@
+from string import ascii_uppercase
+from random import choice 
+
 def make_grid(width, height):
     """
     creates a grid that will hold all 
     the tiles for the boggle game 
     """
-    return {(row, col): ' ' for row in range(height)
+    return {(row, col): choice(ascii_uppercase) 
+        for row in range(height)
         for col in range(width)
     }
+
+
+def neighbours_of_position(coords):
+    """
+    get neighbours of a give position
+    """
+    row = coords[0]
+    col = coords[1]
+
+    # assign each of the neighbours
+    # top left to top right
+
+    top_left = (row - 1, col - 1)
+    top_center = (row - 1, col)
+    top_right = (row - 1, col + 1)
+
+    # left to right
+    left = (row, col - 1)
+    # the row, col coordinates passed to this
+    # function are situated here
+    right = (row, col + 1)
+
+    # bottom left to bottom right
+    bottom_left = (row + 1, col - 1)
+    bottom_center = (row + 1, col + 1)
+    bottom_right = (row + 1, col + 1)
+
+    return [top_left, top_center, top_right,
+                left, right,
+                bottom_left, bottom_center, 
+                bottom_right ]
